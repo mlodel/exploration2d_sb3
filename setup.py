b8f2f40b5b2dea73b32b341401165727accf4c43
Max Lodel <max.lodel@gmail.com>
2022-10-30 14:25:50 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     name='exploration2d_sb3',
     packages=find_packages(),
     version='0.0.1',
-    install_requires=['gym', 'matplotlib', 'pybullet', 'stable-baselines3'])
+#    install_requires=['gym', 'matplotlib', 'pybullet', 'stable-baselines3']
+)
